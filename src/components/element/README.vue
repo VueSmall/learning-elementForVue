f9663fae1e512844cec98aba7ebc46bf329b27b3
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-06-17 22:02:49 +0800

    Create README.vue
    README

---
+++ src/components/element/README.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="readme">
+        <p>README.md</p>
+        <h3>
+            这几天闲着没事干,element组件过一遍,看看哪里需要注意。
+        </h3>
+    </div>
+</template>
+
+<style scoped>
+#readme{
+    text-align: center;
+    width: 100%;
+}
+</style>
