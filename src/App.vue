ea5e5379569ba666deda324d04c5265eedd3ea5c
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-06-18 00:41:24 +0800

    Create App.vue
    App.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+<template>
+  <div id="main">
+      <router-link to="/">README</router-link>
+      <router-link to="/Button">Button按钮</router-link>
+      <router-link to="/Border">Border边框</router-link>
+      <router-view></router-view>
+  </div>
+</template>
+<style>
+
+</style>
