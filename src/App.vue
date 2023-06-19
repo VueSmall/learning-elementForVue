62807d4049856dfdd86f100cbd59100898dfa1d6
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-06-19 23:21:14 +0800

    Update App.vue
    App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,17 @@
       <router-link to="/">README</router-link>
       <router-link to="/Button">Button按钮</router-link>
       <router-link to="/Border">Border边框</router-link>
+      <router-link to="/Color">Color色彩</router-link>
+      <router-link to="/Container">Container 布局容器</router-link>
       <router-view></router-view>
   </div>
 </template>
 <style>
-
+a{
+    margin-left: 1vh;
+    margin-right: 1vh;
+    padding-top: 0.3vh;
+    padding-bottom: 0.3vh;
+    text-decoration: none;
+}
 </style>
